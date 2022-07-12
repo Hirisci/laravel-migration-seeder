634b829bf7835020c81a11fe15c360fc2381277f
Hirisci <www.alan92@icloud.com>
2022-07-12 14:57:02 +0200

    add seed

--- database/seeds/TrainTableSeeder.php
+++ database/seeds/TrainTableSeeder.php
@@ -12,26 +12,30 @@ class TrainTableSeeder extends Seeder
      * @return void
      */
     public function run(Faker $faker)
-    {
-        $train = new Train();
-        $train->agency = $faker->company() ;
-        $train->departure_station= $faker->city();
-        $train->arrival_station= $faker->city();
+    {   
 
-        //Start point of our date range.
-        $start = strtotime("10 may 2022");
+        for($i=0; $i<=50; $i++){
 
-        //End point of our date range.
-        $end = strtotime("22 december 2022");
+            $train = new Train();
+            $train->agency = $faker->company() ;
+            $train->departure_station= $faker->city();
+            $train->arrival_station= $faker->city();
+            
+            //Start point of our date range.
+            $start = strtotime("10 may 2022");
+            
+            //End point of our date range.
+            $end = strtotime("22 december 2022");
 
-        //Custom range.
-        $timestamp = mt_rand($start, $end);
-
-        //Print it out.               
-        $train->departure_time=   date("Y-m-d", $timestamp);
-        $train->arrival_time= date("Y-m-d", $timestamp) ;
-        $train->code_train= $faker->postcode() ;
-        $train->carriages= rand(4,10);
-        $train->save();
+            //Custom range.
+            $timestamp = mt_rand($start, $end);
+            
+            //Print it out.               
+            $train->departure_time=   date("Y-m-d", $timestamp);
+            $train->arrival_time= date("Y-m-d", $timestamp) ;
+            $train->code_train= $faker->postcode() ;
+            $train->carriages= rand(4,10);
+            $train->save();
+        }
     }
 }
