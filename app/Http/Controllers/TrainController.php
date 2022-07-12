cdaf38ac0f2d85575c2bab130c9dd0b433ffbea2
Hirisci <www.alan92@icloud.com>
2022-07-12 15:14:24 +0200

    push

---
+++ app/Http/Controllers/TrainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Train;
+
+
+class TrainController extends Controller
+{
+    public function index(){
+
+        $trains = "öre?";
+        //Train::where('departure_time', date("Y-m-d"))->get();
+
+        return view('home',compact($trains));
+    }
+}
